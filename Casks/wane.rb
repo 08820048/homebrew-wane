f14dd892cd4b83e74d425d89e186355b0d4d582e
xuyi <ilikexff@gmail.com>
2026-06-15 12:42:53 +0800

    Add Wane cask

---
+++ Casks/wane.rb
@@ -0,0 +1,14 @@
+cask "wane" do
+  version :latest
+  sha256 :no_check
+
+  url "https://github.com/08820048/Wane/releases/latest/download/Wane.dmg",
+      verified: "github.com/08820048/Wane/"
+  name "Wane"
+  desc "Ambient progress bars for the current day, week, month, and year"
+  homepage "https://github.com/08820048/Wane"
+
+  depends_on macos: ">= :ventura"
+
+  app "Wane.app"
+end
---
+++ README.md
@@ -0,0 +1,8 @@
+# Homebrew Tap for Wane
+
+Install Wane with:
+
+```sh
+brew tap 08820048/wane
+brew install --cask wane
+```
